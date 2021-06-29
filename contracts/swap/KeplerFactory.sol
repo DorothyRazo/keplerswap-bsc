730655aabda7cc26ad67fbd8a948c75853843119
RustyHowellss <RustyHowellss@outlook.com>
2021-06-29 19:44:38 +0800

    add swap

---
+++ contracts/swap/KeplerFactory.sol
@@ -0,0 +1,173 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.2;
+
+import './KeplerPair.sol';
+import '../libraries/KeplerLibrary.sol';
+import '../interfaces/IKeplerPair.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+contract KeplerFactory is Ownable {
+    using SafeMath  for uint;
+
+    bytes32 public constant INIT_CODE_PAIR_HASH = keccak256(abi.encodePacked(type(KeplerPair).creationCode));
+
+    address public feeTo;
+
+    mapping(address => mapping(address => address)) public getPair;
+    uint public allPairsLength;
+    bool public whiteListAvaliable;
+    mapping(address => bool) whiteList;
+    uint defaultTransferFee;
+    mapping(address => uint) tokenTransferFee;
+    mapping(address => uint) relateTransferFee;
+
+    uint256 public currentSnapshotId;
+    mapping(address => mapping(address => uint256)) userSnapshotId;
+    mapping(address => mapping(address => uint256)) userSnapshotAmount;
+    mapping(address => mapping(address => uint256)) pairSnapshotPrice;
+    address snapshotCreateCaller;
+
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
+
+    constructor() {
+    }
+
+    function setSnapshotCreateCaller(address _snapshotCreateCaller) external onlyOwner {
+        snapshotCreateCaller = _snapshotCreateCaller;
+    }
+
+    function setDefaultTransferFee(uint _defaultTransferFee) external onlyOwner {
+        defaultTransferFee = _defaultTransferFee;
+    }
+
+    function setTokenTransferFee(address token, uint256 fee) external onlyOwner {
+        tokenTransferFee[token] = fee; 
+    }
+
+    function setRelateTransferFee(address token, uint256 fee) external onlyOwner {
+        relateTransferFee[token] = fee;
+    }
+
+    function getTransferFee(address[] memory tokens) external view returns (uint[] memory) {
+        uint[] memory fees = new uint[](tokens.length);
+        for (uint i = 0; i < tokens.length - 1; i ++) {
+            if (tokenTransferFee[tokens[i]] != 0) {
+                fees[i] = tokenTransferFee[tokens[i]];
+            } else if (relateTransferFee[tokens[i+1]] != 0) {
+                fees[i] = relateTransferFee[tokens[i+1]];
+            } else {
+                fees[i] = defaultTransferFee;
+            }
+        }
+        return fees;
+    }
+
+    function expectPairFor(address token0, address token1) external view returns (address) {
+        return KeplerLibrary.pairFor(address(this), token0, token1);
+    }
+
+    function isWhiteList(address token0, address token1) external view returns (bool) {
+        address pair = KeplerLibrary.pairFor(address(this), token0, token1);
+        return isWhiteList(pair);
+    }
+
+    function isWhiteList(address pair) internal view returns(bool) {
+        if (whiteListAvaliable) {
+            return whiteList[pair];
+        } else {
+            return true;
+        }
+    }
+
+    function createPair(address tokenA, address tokenB) public returns (address pair) {
+        require(tokenA != tokenB, 'Kepler: IDENTICAL_ADDRESSES');
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'Kepler: ZERO_ADDRESS');
+        require(getPair[token0][token1] == address(0), 'Kepler: PAIR_EXISTS'); // single check is sufficient
+        bytes memory bytecode = type(KeplerPair).creationCode;
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1));
+        assembly {
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+        require(isWhiteList(pair), "Kepler: NOT WHITELIST");
+        IKeplerPair(pair).initialize(token0, token1);
+        getPair[token0][token1] = pair;
+        getPair[token1][token0] = pair; // populate mapping in the reverse direction
+        allPairsLength = allPairsLength + 1;
+        emit PairCreated(token0, token1, pair, allPairsLength);
+    }
+
+    function setFeeTo(address _feeTo) external onlyOwner {
+        feeTo = _feeTo;
+    }
+
+    function createSnapshot(address pair, uint256 id) external {
+        require(msg.sender == snapshotCreateCaller, "only snapshotCreateCaller can do this");
+        currentSnapshotId = id; 
+        address token0 = IKeplerPair(pair).token0();
+        address token1 = IKeplerPair(pair).token1();
+        (uint112 reserve0, uint112 reserve1,) = IKeplerPair(pair).getReserves();
+        if (reserve0 != 0) {
+            pairSnapshotPrice[pair][token0] = IERC20(token0).balanceOf(pair).mul(1e18).div(reserve0);
+        }
+        if (reserve1 != 0) {
+            pairSnapshotPrice[pair][token1] = IERC20(token1).balanceOf(pair).mul(1e18).div(reserve1);
+        }
+    }
+
+    function getSnapshotPrice(IKeplerPair pair) public view returns(uint price0, uint price1) {
+        address token0 = pair.token0();
+        address token1 = pair.token1();
+        if (currentSnapshotId == 0) {
+            (uint reserve0, uint reserve1,) = pair.getReserves();
+            if (reserve0 != 0) {
+                price0 = IERC20(token0).balanceOf(address(pair)).mul(1e18).div(reserve0);
+            }
+            if (reserve1 != 0) {
+                price1 = IERC20(token1).balanceOf(address(pair)).mul(1e18).div(reserve1);
+            }
+        } else {
+            price0 = pairSnapshotPrice[address(pair)][token0];
+            price1 = pairSnapshotPrice[address(pair)][token1];
+        }
+    }
+
+    function getSnapshotBalance(IKeplerPair pair, address user) public view returns (uint) {
+        if (currentSnapshotId == 0 || userSnapshotId[address(pair)][user] != currentSnapshotId) {
+            return pair.balanceOf(user);
+        } else if (userSnapshotId[address(pair)][user] == currentSnapshotId) {
+            return userSnapshotAmount[address(pair)][user];
+        }
+    }
+
+    function getUserSnapshot(address pair, address user) external view returns (uint256, uint256) {
+        uint balance = getSnapshotBalance(IKeplerPair(pair), user);
+        (uint price0, uint price1) = getSnapshotPrice(IKeplerPair(pair));
+        return (balance.mul(price0).div(1e18), balance.mul(price1).div(1e18));
+    }
+
+    function _beforeTokenTransfer(address token0, address token1, address from, address to, uint256 amount) external {
+        if (false) {
+            amount;
+        }
+        address pair = getPair[token0][token1];
+        if (pair == address(0)) {
+            return;
+        }
+        require(msg.sender == pair, "only pair can do this");
+        if (currentSnapshotId == 0) {
+            return;
+        }
+        if (userSnapshotId[pair][from] < currentSnapshotId) {
+            userSnapshotAmount[pair][from] = IKeplerPair(pair).balanceOf(from);
+            userSnapshotId[pair][from] = currentSnapshotId;
+        }
+        if (userSnapshotId[pair][to] < currentSnapshotId) {
+            userSnapshotAmount[pair][to] = IKeplerPair(pair).balanceOf(to);
+            userSnapshotId[pair][to] = currentSnapshotId;
+        }
+    }
+}
---
+++ contracts/swap/KeplerRouter.sol
@@ -0,0 +1,286 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.7.2;
+
+import '../interfaces/IKeplerRouter.sol';
+import '../interfaces/IKeplerFactory.sol';
+import '../libraries/KeplerLibrary.sol';
+import '../libraries/TransferHelper.sol';
+import '../interfaces/IWETH.sol';
+import '../interfaces/IFeeDispatcher.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import 'hardhat/console.sol';
+/*
+import '../interfaces/IERC20.sol';
+*/
+contract KeplerRouter is IKeplerRouter {
+    using SafeMath for uint256;
+
+    address public immutable override factory;
+    address public immutable override WETH;
+
+    modifier ensure(uint deadline) {
+        require(deadline >= block.timestamp, 'BabyRouter: EXPIRED');
+        _;
+    }
+
+    constructor(address _factory, address _WETH) {
+        factory = _factory;
+        WETH = _WETH;
+    }
+
+    receive() external payable {
+        assert(msg.sender == WETH); // only accept ETH via fallback from the WETH contract
+    }
+
+    // **** ADD LIQUIDITY ****
+    function _addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin
+    ) private returns (uint amountA, uint amountB) {
+        // create the pair if it doesn't exist yet
+        if (IKeplerFactory(factory).getPair(tokenA, tokenB) == address(0)) {
+            IKeplerFactory(factory).createPair(tokenA, tokenB);
+        }
+        (uint reserveA, uint reserveB) = KeplerLibrary.getReserves(factory, tokenA, tokenB);
+        if (reserveA == 0 && reserveB == 0) {
+            (amountA, amountB) = (amountADesired, amountBDesired);
+        } else {
+            uint amountBOptimal = KeplerLibrary.quote(amountADesired, reserveA, reserveB);
+            if (amountBOptimal <= amountBDesired) {
+                require(amountBOptimal >= amountBMin, 'KeplerRouter: INSUFFICIENT_B_AMOUNT');
+                (amountA, amountB) = (amountADesired, amountBOptimal);
+            } else {
+                uint amountAOptimal = KeplerLibrary.quote(amountBDesired, reserveB, reserveA);
+                assert(amountAOptimal <= amountADesired);
+                require(amountAOptimal >= amountAMin, 'KeplerRouter: INSUFFICIENT_A_AMOUNT');
+                (amountA, amountB) = (amountAOptimal, amountBDesired);
+            }
+        }
+    }
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external override ensure(deadline) returns (uint amountA, uint amountB, uint liquidity) {
+        (amountA, amountB) = _addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin);
+        address pair = KeplerLibrary.pairFor(factory, tokenA, tokenB);
+        TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
+        TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        liquidity = IKeplerPair(pair).mint(to);
+    }
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external override payable ensure(deadline) returns (uint amountToken, uint amountETH, uint liquidity) {
+        (amountToken, amountETH) = _addLiquidity(
+            token,
+            WETH,
+            amountTokenDesired,
+            msg.value,
+            amountTokenMin,
+            amountETHMin
+        );
+        address pair = KeplerLibrary.pairFor(factory, token, WETH);
+        TransferHelper.safeTransferFrom(token, msg.sender, pair, amountToken);
+        IWETH(WETH).deposit{value: amountETH}();
+        assert(IWETH(WETH).transfer(pair, amountETH));
+        liquidity = IKeplerPair(pair).mint(to);
+        if (msg.value > amountETH) TransferHelper.safeTransferETH(msg.sender, msg.value - amountETH); // refund dust eth, if any
+    }
+
+    // **** REMOVE LIQUIDITY ****
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) public override ensure(deadline) returns (uint amountA, uint amountB) {
+        address pair = KeplerLibrary.pairFor(factory, tokenA, tokenB);
+        IKeplerPair(pair).transferFrom(msg.sender, pair, liquidity); // send liquidity to pair
+        (uint amount0, uint amount1) = IKeplerPair(pair).burn(to);
+        (address token0,) = KeplerLibrary.sortTokens(tokenA, tokenB);
+        (amountA, amountB) = tokenA == token0 ? (amount0, amount1) : (amount1, amount0);
+        require(amountA >= amountAMin, 'KeplerRouter: INSUFFICIENT_A_AMOUNT');
+        require(amountB >= amountBMin, 'KeplerRouter: INSUFFICIENT_B_AMOUNT');
+    }
+    function removeLiquidityETH(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) public override ensure(deadline) returns (uint amountToken, uint amountETH) {
+        (amountToken, amountETH) = removeLiquidity(
+            token,
+            WETH,
+            liquidity,
+            amountTokenMin,
+            amountETHMin,
+            address(this),
+            deadline
+        );
+        TransferHelper.safeTransfer(token, to, amountToken);
+        IWETH(WETH).withdraw(amountETH);
+        TransferHelper.safeTransferETH(to, amountETH);
+    }
+
+    // **** SWAP ****
+    // requires the initial amount to have already been sent to the first pair
+    function _swap(uint[] memory amounts, address[] memory path, address _to) private {
+        for (uint i; i < path.length - 1; i++) {
+            (address input, address output) = (path[i], path[i + 1]);
+            (address token0,) = KeplerLibrary.sortTokens(input, output);
+            uint amountOut = amounts[i + 1];
+            (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
+            address to = i < path.length - 2 ? KeplerLibrary.pairFor(factory, output, path[i + 2]) : _to;
+            IKeplerPair(KeplerLibrary.pairFor(factory, input, output)).swap(amount0Out, amount1Out, to);
+        }
+    }
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external override ensure(deadline) returns (uint[] memory amounts) {
+        uint256[] memory fees = IKeplerFactory(factory).getTransferFee(path);
+        amounts = KeplerLibrary.getAmountsOut(factory, amountIn, path, fees);
+        address feeTo = IKeplerFactory(factory).feeTo();
+        for (uint i = 0; i < fees.length - 1; i ++) {
+            if (fees[i] == 0) continue;
+            uint fee = amounts[i].mul(fees[i]).div(1000);
+            console.log("fee amount: ", fee);
+            TransferHelper.safeTransferFrom(path[i], msg.sender, address(this), fee);
+            IERC20(path[i]).approve(feeTo, fee);
+            IFeeDispatcher(feeTo).doHardWork(KeplerLibrary.pairFor(factory, path[i], path[i + 1]), path[i], msg.sender, fee);
+            amounts[i] = amounts[i].sub(fee);        
+        }
+        require(amounts[amounts.length - 1] >= amountOutMin, 'KeplerRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(path[0], msg.sender, KeplerLibrary.pairFor(factory, path[0], path[1]), amounts[0]);
+        _swap(amounts, path, to);
+    }
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external override ensure(deadline) returns (uint[] memory amounts) {
+        uint256[] memory fees = IKeplerFactory(factory).getTransferFee(path);
+        amounts = KeplerLibrary.getAmountsIn(factory, amountOut, path, fees);
+        address feeTo = IKeplerFactory(factory).feeTo();
+        for (uint i = 0; i < fees.length - 1; i ++) {
+            IERC20(path[i]).approve(feeTo, fees[i]);
+            IFeeDispatcher(feeTo).doHardWork(KeplerLibrary.pairFor(factory, path[i], path[i + 1]), path[i], msg.sender, fees[i]);
+            amounts[i] = amounts[i].sub(fees[i]);        
+        }
+        require(amounts[0] <= amountInMax, 'KeplerRouter: EXCESSIVE_INPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(path[0], msg.sender, KeplerLibrary.pairFor(factory, path[0], path[1]), amounts[0]);
+        _swap(amounts, path, to);
+    }
+    function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        override
+        payable
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[0] == WETH, 'KeplerRouter: INVALID_PATH');
+        uint256[] memory fees = IKeplerFactory(factory).getTransferFee(path);
+        amounts = KeplerLibrary.getAmountsOut(factory, msg.value, path, fees);
+        address feeTo = IKeplerFactory(factory).feeTo();
+        for (uint i = 0; i < fees.length - 1; i ++) {
+            IERC20(path[i]).approve(feeTo, fees[i]);
+            IFeeDispatcher(feeTo).doHardWork(KeplerLibrary.pairFor(factory, path[i], path[i + 1]), path[i], msg.sender, fees[i]);
+            amounts[i] = amounts[i].sub(fees[i]);        
+        }
+        require(amounts[amounts.length - 1] >= amountOutMin, 'KeplerRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        IWETH(WETH).deposit{value: amounts[0]}();
+        assert(IWETH(WETH).transfer(KeplerLibrary.pairFor(factory, path[0], path[1]), amounts[0]));
+        _swap(amounts, path, to);
+    }
+    function swapTokensForExactETH(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        override
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[path.length - 1] == WETH, 'BabyRouter: INVALID_PATH');
+        uint256[] memory fees = IKeplerFactory(factory).getTransferFee(path);
+        amounts = KeplerLibrary.getAmountsIn(factory, amountOut, path, fees);
+        address feeTo = IKeplerFactory(factory).feeTo();
+        for (uint i = 0; i < fees.length - 1; i ++) {
+            IERC20(path[i]).approve(feeTo, fees[i]);
+            IFeeDispatcher(feeTo).doHardWork(KeplerLibrary.pairFor(factory, path[i], path[i + 1]), path[i], msg.sender, fees[i]);
+            amounts[i] = amounts[i].sub(fees[i]);        
+        }
+        require(amounts[0] <= amountInMax, 'KeplerRouter: EXCESSIVE_INPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(path[0], msg.sender, KeplerLibrary.pairFor(factory, path[0], path[1]), amounts[0]);
+        _swap(amounts, path, address(this));
+        IWETH(WETH).withdraw(amounts[amounts.length - 1]);
+        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
+    }
+    function swapExactTokensForETH(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        override
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[path.length - 1] == WETH, 'KeplerRouter: INVALID_PATH');
+        uint256[] memory fees = IKeplerFactory(factory).getTransferFee(path);
+        amounts = KeplerLibrary.getAmountsOut(factory, amountIn, path, fees);
+        address feeTo = IKeplerFactory(factory).feeTo();
+        for (uint i = 0; i < fees.length - 1; i ++) {
+            IERC20(path[i]).approve(feeTo, fees[i]);
+            IFeeDispatcher(feeTo).doHardWork(KeplerLibrary.pairFor(factory, path[i], path[i + 1]), path[i], msg.sender, fees[i]);
+            amounts[i] = amounts[i].sub(fees[i]);        
+        }
+        require(amounts[amounts.length - 1] >= amountOutMin, 'KeplerRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(path[0], msg.sender, KeplerLibrary.pairFor(factory, path[0], path[1]), amounts[0]);
+        _swap(amounts, path, address(this));
+        IWETH(WETH).withdraw(amounts[amounts.length - 1]);
+        TransferHelper.safeTransferETH(to, amounts[amounts.length - 1]);
+    }
+    function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        override
+        payable
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[0] == WETH, 'KeplerRouter: INVALID_PATH');
+        uint256[] memory fees = IKeplerFactory(factory).getTransferFee(path);
+        amounts = KeplerLibrary.getAmountsIn(factory, amountOut, path, fees);
+        address feeTo = IKeplerFactory(factory).feeTo();
+        for (uint i = 0; i < fees.length - 1; i ++) {
+            IERC20(path[i]).approve(feeTo, fees[i]);
+            IFeeDispatcher(feeTo).doHardWork(KeplerLibrary.pairFor(factory, path[i], path[i + 1]), path[i], msg.sender, fees[i]);
+            amounts[i] = amounts[i].sub(fees[i]);        
+        }
+        require(amounts[0] <= msg.value, 'KeplerRouter: EXCESSIVE_INPUT_AMOUNT');
+        IWETH(WETH).deposit{value: amounts[0]}();
+        assert(IWETH(WETH).transfer(KeplerLibrary.pairFor(factory, path[0], path[1]), amounts[0]));
+        _swap(amounts, path, to);
+        if (msg.value > amounts[0]) TransferHelper.safeTransferETH(msg.sender, msg.value - amounts[0]); // refund dust eth, if any
+    }
+}
